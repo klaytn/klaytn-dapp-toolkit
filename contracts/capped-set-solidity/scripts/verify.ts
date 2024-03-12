c58eff07e7f988b5e7499048991e0bc651acbc51
Nguyễn Đình Trường <ndtr2000@gmail.com>
2024-03-12 13:05:48 +0700

    Init capped set contract

---
+++ contracts/capped-set-solidity/scripts/verify.ts
@@ -0,0 +1,22 @@
+import * as hre from "hardhat";
+import * as contracts from "../contracts.json";
+import { Config } from "./config";
+
+async function main() {
+    try {
+        await hre.run("verify:verify", {
+            address: contracts.cappedSet,
+            constructorArguments: [Config.capacity],
+            hre,
+        });
+    } catch (err) {
+        console.log("err >>", err);
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
