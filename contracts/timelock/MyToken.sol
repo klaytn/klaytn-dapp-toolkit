e214d245a901845c96c64eb361218cf32fa14b2a
afrokayie <oluwolekayode61@gmail.com>
2024-03-19 22:29:55 +0100

    corrections

--- contracts/timelock/MyToken.sol
+++ contracts/timelock/MyToken.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
         _mint(msg.sender, initialSupply);
     }
 }
