f092407aac11f5619dcc3a37000287c427a42bd8
Paul Elisha <ireajao@yahoo.com>
2024-05-01 14:38:15 +0100

    updated folder

---
+++ contracts/CoinFlip/contract/CoinFlip.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import { VRFConsumerBase } from "@bisonai/orakl-contracts/src/v0.1/VRFConsumerBase.sol";
+import { IVRFCoordinator } from "@bisonai/orakl-contracts/src/v0.1/interfaces/IVRFCoordinator.sol";
+
+contract CoinFlip is VRFConsumerBase {
+
+    enum CoinFlipChoice {
+        HEADS,
+        TAILS
+    }
+
+    struct CoinFlipStatus {
+        uint256 fees;
+        uint256 randomWord;
+        address player;
+        bool didWin;
+        bool fulfilled;
+        CoinFlipChoice choice;
+    }
+
+    error CoinFlip__EntryFeesNotEnough();
+    error CoinFlip__RequestNotFound();
+
+    mapping (uint256 => CoinFlipStatus) public s_status;
+
+    uint256 public s_entryFees = 1 ether;
+
+    uint256 private sRandomWord;
+    IVRFCoordinator immutable i_vrfCoordinator;
+
+    address private vrfAddress;
+    uint64 private s_subscriptionId;
+    uint32 private constant s_gasLane = 100000;
+    uint32 private constant s_numWords = 1;
+    bytes32 private constant s_keyHash =
+        0xd9af33106d664a53cb9946df5cd81a30695f5b72224ee64e798b278af812779c;
+
+    constructor(address coordinator) VRFConsumerBase(coordinator) {
+        i_vrfCoordinator = IVRFCoordinator(coordinator);
+    }
+
+    function flip(CoinFlipChoice choice) external payable returns(uint256 requestId){
+        if(msg.value < s_entryFees) {
+            revert CoinFlip__EntryFeesNotEnough();
+        }
+
+        requestId = i_vrfCoordinator.requestRandomWords(
+            s_keyHash,
+            s_subscriptionId,
+            s_gasLane,
+            s_numWords
+        );
+
+        s_status[requestId] = CoinFlipStatus(
+            {
+            fees: s_gasLane,
+            randomWord: 0,
+            player: msg.sender,
+            didWin: false,
+            fulfilled: false,
+            choice: choice
+            }
+        );
+        
+        return requestId;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        if(s_status[requestId].fees < 0) {
+            revert CoinFlip__RequestNotFound();
+        }
+
+        s_status[requestId].fulfilled = true;
+        s_status[requestId].randomWord = randomWords[0];
+
+        CoinFlipChoice result = CoinFlipChoice.HEADS;
+
+        if(randomWords[0] % 2 == 0) {
+            result = CoinFlipChoice.TAILS; // If the random Word is even, it is tails
+        }
+
+        if(s_status[requestId].choice == result) {
+            s_status[requestId].didWin = true;
+            (bool success, ) = payable(s_status[requestId].player).call{value: s_entryFees * 2}("");
+            require(success, "Transation failed");
+        }
+    }
+
+    function getStatus(uint256 requestId) public view returns(CoinFlipStatus memory) {
+        return s_status[requestId];
+    }
+
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+}
---
+++ contracts/CoinFlip/deploy/deployCoinFlip.s.js
@@ -0,0 +1,47 @@
+const { ethers, network, run } = require("hardhat");
+
+const deploy = async () => {
+
+    const args = ["0x6B4c0b11bd7fE1E9e9a69297347cFDccA416dF5F"]
+
+    const CoinFlip = await ethers.deployContract("CoinFlip", args);
+    console.log("Deploying contract...");
+    await CoinFlip.waitForDeployment(6);
+    console.log(`Contract Deployed to: ${CoinFlip.target}`)
+    console.log(network.config)
+
+    // if (network.config.chainId === 1001 && process.env.KLAYTN_RPC) {
+    //     await CoinFlip.waitForDeployment(6);
+    //     await verify(CoinFlip.target, args)
+    // } else {
+    //     console.log("Contract cannot be verified on Hardhat Network")
+    // }
+
+    const sendVal = ethers.parseEther("1");
+    const flip = await CoinFlip.flip(0, { value: sendVal });
+    await flip.wait(3);
+    console.log(`Flipped!`);
+}
+
+// Use Klaytn custom chain config to verify your contract
+
+// const verify = async (contractAddress, args) => {
+//     console.log("Verifying contract....")
+//     try {
+//         await run("verify:verify", {
+//             address: contractAddress,
+//             constructorArgs: args
+//         })
+//     } catch (error) {
+//         if (error.message.toLowerCase().includes("already verified")) {
+//             console.log("Already verified...!")
+//         } else {
+//             console.log(error);
+//         }
+//     }
+// }
+
+deploy().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
