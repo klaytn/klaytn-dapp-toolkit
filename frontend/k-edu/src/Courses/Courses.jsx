94ba6d8dfefb9a84630e083c5eab9ebc5836fdf8
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-06-16 09:41:42 +0100

    Optimizing course category toggling

--- frontend/k-edu/src/Courses/Courses.jsx
+++ frontend/k-edu/src/Courses/Courses.jsx
@@ -9,58 +9,72 @@ export default function Courses(props) {
 
   useEffect(() => {
     const categories = document.querySelectorAll(".categories");
-    const toggle = document.querySelectorAll(".category");
+    const toggles = document.querySelectorAll(".category");
 
     categories.forEach((category) => {
       category.style.display = "none";
     });
 
     categories[0].style.display = "block";
-    toggle[0].style.color = "rgb(155, 9, 29)";
-  })
+    toggles[0].style.color = "rgb(155, 9, 29)";
 
-  // The function controls the course toggle action
-  const toggleCourse = () => {
-    const toggles = document.querySelectorAll(".category");
-    const categories = document.querySelectorAll(".categories");
+    const handleToggle = (index) => {
+      // Changes the toggle color on clicking it
+      toggles.forEach((t) => {
+        t.style.color = "rgb(34, 32, 32";
+      });
+      toggles[index].style.color = "rgb(155, 9, 29)";
+      // Switches the category on clicking a toggle
+      categories.forEach((category) => {
+        category.style.display = "none";
+      });
+      categories[index].style.display = "block";
+    };
 
     toggles.forEach((toggle, index) => {
-      // Changes the toggle color on clicking it
-      toggle.addEventListener("click", () => {
-        toggles.forEach((toggle) => {
-          toggle.style.color = "rgb(34, 32, 32)";
-        });
-        toggle.style.color = "rgb(155, 9, 29)";
-        // Switches the category on clicking a toggle
-        categories.forEach((category) => {
-          category.style.display = "none";
-        });
-        categories[index].style.display = "block";
-      });
+      toggle.addEventListener("click", () => handleToggle(index));
     });
-  };
+  });
+
+  // The function controls the course toggle action
+  // const toggleCourse = () => {
+  //   const toggles = document.querySelectorAll(".category");
+  //   const categories = document.querySelectorAll(".categories");
+
+  //   toggles.forEach((toggle, index) => {
+  //     // Changes the toggle color on clicking it
+  //     toggle.addEventListener("click", () => {
+  //       toggles.forEach((toggle) => {
+  //         toggle.style.color = "rgb(34, 32, 32)";
+  //       });
+  //       toggle.style.color = "";
+  //       // Switches the category on clicking a toggle
+  //       categories.forEach((category) => {
+  //         category.style.display = "none";
+  //       });
+  //       categories[index].style.display = "block";
+  //     });
+  //   });
+  // };
   return (
     <React.Fragment>
       <div className="courses">
         <div className="coursesContainer">
           <div className="courseCategory">
             <ul>
-              <li onClick={toggleCourse} className="category">
-                Free
-              </li>
-              <li onClick={toggleCourse} className="category">
-                Paid
-              </li>
-              <li onClick={toggleCourse} className="category">
-                Advanced
-              </li>
+              <li className="category">Free</li>
+              <li className="category">Paid</li>
+              <li className="category">Advanced</li>
             </ul>
           </div>
           <section className="freeCourses categories">
             <FreeCourses handleCourseId={handleCourseId} />
           </section>
           <section className="paidCourses categories">
-            <PaidCourses handleCourseId={handleCourseId} klayBalance={klayBalance} />
+            <PaidCourses
+              handleCourseId={handleCourseId}
+              klayBalance={klayBalance}
+            />
           </section>
           <section className="AdvancedCourses categories">
             <AdvanceCourses handleCourseId={handleCourseId} />
